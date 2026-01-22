75643ba5920775db97c5e439a15322d03f8444c9
Apoorv Bansal <apoorvbansal@Apoorvs-MacBook-Pro.local>
2026-01-22 12:02:45 +0100

    m

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettingsm, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import List
 
 class Settings(BaseSettings):
