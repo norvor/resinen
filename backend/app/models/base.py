dd411f74f0d530010d6caa8d687d9997f9aa5d9b
Apoorv Bansal <apoorvbansal@Apoorvs-MacBook-Pro.local>
2026-01-26 09:37:06 +0100

    m

--- backend/app/models/base.py
+++
@@ -1,5 +0,0 @@
-# Imports for Alembic/SQLModel to see all tables
-from app.models.user import User
-from app.models.engine import Engine, UserEngine
-from app.models.journal import JournalEntry
-from app.models.vault import VaultItem
